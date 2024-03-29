995ba8f77d8ae0420d5a018f101d3637c9470513
Chan Tsz Chung <arthurchan705119@protonmail.com>
2024-03-29 16:11:16 +0000

    Second Draft

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
 import random
 import string
@@ -21,12 +22,21 @@ class Params(BaseModel):
 app = FastAPI()
 
 
+@app.get("/")
+async def index():
+    # Return the HTML file as a response
+    return FileResponse("static/index.html")
+
+
 @app.post("/generate-password")
 async def create_item(params: Params):
+    if params.length < 12:
+        return {"error": "Minimum length for a secure password is 12."}
+
     characters = string.ascii_letters + string.digits + string.punctuation  # includes uppercase letters, lowercase letters, digits, and punctuation symbols
 
     # Generate multiple random characters
-    random_chars = random.choices(characters, k=params.length)  # Generate 5 random characters
+    random_chars = random.choices(characters, k=params.length)
 
     # Concatenate random_chars into a string
     random_string = ''.join(random_chars)
